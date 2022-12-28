e34bd7f6068e466778d327adde7a650929047c20
Antony <guolyxd@163.com>
2022-12-28 15:59:19 +0800

    update controller

--- src/main/java/example/api/rest/controllers/CampaignsController.java
+++ src/main/java/example/api/rest/controllers/CampaignsController.java
@@ -38,44 +38,49 @@ public class CampaignsController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public Page<Campaign> getAllCampaigns(@RequestParam(value="page", required = false, defaultValue = CAMPAIGNS_PAGE_DEFAULT) int page,
-                                          @RequestParam(value="size", required = false, defaultValue = CAMPAIGNS_SIZE_DEFAULT) int size) {
+                                          @RequestParam(value="size", required = false, defaultValue = CAMPAIGNS_SIZE_DEFAULT) int size)
+        {
         LOGGER.info("Entering in rest endpoint to get all campaigns");
         PageRequest pageRequest = PageRequest.of(page, size);
         return campaignsService.getAllCampaigns(pageRequest);
-    }
+        }
 
     @ApiOperation(value = "Get a campaign by id")
     @RequestMapping(value = "/{campaignId}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
-    public Campaign getCampaignById(@PathVariable("campaignId") String campaignId) {
+    public Campaign getCampaignById(@PathVariable("campaignId") String campaignId) 
+       {
         LOGGER.info("Entering in rest endpoint to get a campaign by id");
         return campaignsService.getCampaignById(campaignId);
-    }
+       }
 
     @ApiOperation(value = "Create a new campaign")
     @RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public Campaign createCampaign(@Valid @RequestBody Campaign campaign) {
+    public Campaign createCampaign(@Valid @RequestBody Campaign campaign) 
+      {
         LOGGER.info("Entering in rest endpoint to create a new campaign");
         campaign.setId(sequenceGeneratorService.generateSequence(Campaign.SEQUENCE_NAME));
         return campaignsService.createCampaign(campaign);
-    }
+      }
 
     @ApiOperation(value = "Update an existing campaign by id")
     @RequestMapping(value = "/{campaignId}", method = RequestMethod.PUT)
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public Campaign updateCampaign(@Valid @RequestBody Campaign campaign, @PathVariable("campaignId") String campaignId) {
+    public Campaign updateCampaign(@Valid @RequestBody Campaign campaign, @PathVariable("campaignId") String campaignId) 
+       {
         LOGGER.info("Entering in rest endpoint to update campaign");
         campaign.setId(campaignId);
         return campaignsService.updateCampaign(campaign);
-    }
+       }
 
     @ApiOperation(value = "Delete a campaign by id")
     @RequestMapping(value = "/{campaignId}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCampaign(@PathVariable("campaignId") String campaignId) {
+    public void deleteCampaign(@PathVariable("campaignId") String campaignId) 
+       {
         LOGGER.info("Entering in rest endpoint to delete the campaign");
         campaignsService.deleteCampaignById(campaignId);
-    }
+        }
 
 }
